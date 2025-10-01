3e44230c160235f5e23b925571b9abd1e36edb3f
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2025-10-01 12:19:42 +0500

    Introduction to MongoDB, Connect Node Project With MongoDB

--- controllers/errors.js
+++
@@ -1,4 +0,0 @@
-exports.pageNotFount =  (req, res , next) => {
- res.status(404).render('404');
-
-}
--- utils/pathUtils.js
+++
@@ -1,3 +0,0 @@
-// Core Module
-const path = require('path');
-module.exports = path.dirname(require.main.filename);
